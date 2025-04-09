c20f42ad51682247c323372586855ac65e8510ee
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-09 11:29:45 +0800

    Write questions and answer as json file

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,5 @@
+import json
+
 # ask user for question and choices
 question = input("Enter your quiz question: ")
 
@@ -7,7 +9,12 @@ for letter in ['a', 'b', 'c', 'd']:
 
 # ask for correct answer
 while True:
-        answer = input("Enter the correct answer (a/b/c/d): ").lower()
-        if answer in choices:
-            break
-        print("Invalid input. Please enter one of a, b, c, or d.")
+    answer = input("Enter the correct answer (a/b/c/d): ").lower()
+    if answer in choices:
+        break
+    print("Invalid input. Please enter one of a, b, c, or d.")
+
+# save the question, choices, and answer key as json
+filename = "input_questions_answer.jsonl"
+with open(filename, "a", encoding="utf-8") as file:
+    file.write()
